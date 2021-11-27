1e21a1eef0bc06376e510495d31b11c325f67727
MB-study <guomiao526@gmail.com>
2021-11-27 14:15:12 +0800

    a

---
+++ src/test/java/case02/Test1.java
@@ -0,0 +1,28 @@
+package case02;
+
+import java.util.Arrays;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import com.study.spring.case01.TurnOn;
+import com.study.spring.case02.Pager;
+import com.study.spring.case02.Pager2;
+import com.study.spring.case02.Pager3;
+
+
+public class Test1 {
+	// 載入配置檔會將所有的 <bean scope="singleton"> 標籤內實例化，無論程式碼是否有用到
+	// <bean scope="prototype"> 則不會實例化
+	public static void main(String[] args) {
+		ApplicationContext ctx = new  ClassPathXmlApplicationContext("applicationContext2.xml");
+		Pager pager = ctx.getBean("pager", Pager.class);
+		System.out.println(pager);
+		
+		Pager2 pager2 = ctx.getBean("pager2", Pager2.class);
+		System.out.println(pager2);
+		Pager3 pager3 = ctx.getBean("pager3", Pager3.class);
+		System.out.println(pager3);
+	}
+	
+}
