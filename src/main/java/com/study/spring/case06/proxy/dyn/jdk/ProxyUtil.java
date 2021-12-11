152ccee5eb10bc97fd69542d2ae6f9dcde6820e1
MB-study <guomiao526@gmail.com>
2021-12-11 11:23:25 +0800

    master

---
+++ src/main/java/com/study/spring/case06/proxy/dyn/jdk/ProxyUtil.java
@@ -0,0 +1,37 @@
+package com.study.spring.case06.proxy.dyn.jdk;
+
+import java.lang.reflect.InvocationHandler;
+import java.lang.reflect.Method;
+import java.lang.reflect.Proxy;
+
+public class ProxyUtil {
+	private Object object;
+	
+	public ProxyUtil (Object object) {
+		this.object = object;
+	}
+	/*	16-31 方法實現參數 */
+	
+	public Object getProxy() {
+		// 1. 類別載入器
+		ClassLoader loader = object.getClass().getClassLoader();		
+		// 2. 目標所實作的介面
+		Class[] interfaces = object.getClass().getInterfaces();
+		// 3. 處理代理的實現
+		// 介面: Interface InvocationHandler
+		/*Object(需要回傳值) invoke (Object proxy, Method method, Object[] args) throws Throwable  */
+		InvocationHandler handler = (Object proxy, Method method, Object[] args) -> {
+			Object ressult = null;
+			// 公用方法
+			System.out.println("紀錄 log 1");
+			// 業務邏輯 (代理方法的呼叫)
+			ressult = method.invoke(object, args);
+			
+			return ressult;
+		};
+		// 4. 透過 Proxy.newProxyInstance 實現代理機制
+		Object proxyObj = Proxy.newProxyInstance(loader, interfaces, handler);
+		return proxyObj;
+		
+	}
+}
